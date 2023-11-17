1ef1b9979e6a2458172a41f57598fb27f514bd86
DamienMescudi <dam.mescudi@gmail.com>
2023-11-17 21:16:31 +0100

    GUI update

--- script.py
+++ script.py
@@ -14,24 +14,30 @@ CROSS_OUT = '\033[9m'
 END = '\033[0m'
 
 
-def highlight_differences(original, corrected):
+def highlight_differences(original, corrected, text_widget):
     original_words = original.split()
     corrected_words = corrected.split()
     matcher = difflib.SequenceMatcher(None, original_words, corrected_words)
-    highlighted = ''
 
+    # Commencer à insérer à la fin du widget de texte
+    index = 'end'
     for opcode in matcher.get_opcodes():
         tag, i1, i2, j1, j2 = opcode
         if tag == 'equal':
-            highlighted += ' '.join(original_words[i1:i2]) + ' '
-        elif tag == 'replace':
-            highlighted += RED + CROSS_OUT + ' '.join(original_words[i1:i2]) + END + ' ' + GREEN + ' '.join(corrected_words[j1:j2]) + END + ' '
+            for word in original_words[i1:i2]:
+                text_widget.insert(index, word + " ", 'normal')
+                index = text_widget.index(f"{index}+{len(word) + 1}c")
+        elif tag in ['replace', 'insert']:
+            for word in corrected_words[j1:j2]:
+                text_widget.insert(index, word + " ", 'corrected')
+                index = text_widget.index(f"{index}+{len(word) + 1}c")
         elif tag == 'delete':
-            highlighted += RED + CROSS_OUT + ' '.join(original_words[i1:i2]) + END + ' '
-        elif tag == 'insert':
-            highlighted += GREEN + ' '.join(corrected_words[j1:j2]) + END + ' '
+            continue
+    
+    text_widget.insert(index, "\n\n")
 
-    return highlighted.strip()
+
+
 
 
 
@@ -116,29 +122,95 @@ def on_submit():
     presentation_id = entry_presentation_id.get()
     text_elements = get_presentation_text(service, presentation_id)
 
+def on_submit():
+    presentation_id = entry_presentation_id.get()
+    text_elements = get_presentation_text(service, presentation_id)
+
+    # Commencer l'ajout de texte après l'en-tête ASCII
+    start_index = text_area.index('end-1c')
+    text_area.insert(start_index, "\n")
+
     for element_id, full_text in text_elements.items():
         if full_text.strip():
             corrected_text = correct_text_with_openai(full_text, settings['openai_api_key'])
-            highlighted_diff = highlight_differences(full_text, corrected_text)
+            highlight_differences(full_text, corrected_text, text_area)
             update_slide_text(service, presentation_id, element_id, corrected_text)
-            text_area.insert(tk.INSERT, f"Correction apportée :\n{highlighted_diff}\n\n")
 
-# Configuration de l'interface graphique
+            # Mise à jour de l'affichage après chaque traitement de texte
+            root.update_idletasks()
+
+
+
+
+
+# Fonctions pour gérer le placeholder
+def on_entry_click(event):
+    """Fonction appelée lorsque le champ de saisie est cliqué."""
+    if entry_presentation_id.get() == 'Entrez l\'ID de la présentation...':
+        entry_presentation_id.delete(0, "end") # Supprime le texte du placeholder
+        entry_presentation_id.insert(0, '') # Insère un texte vide
+        entry_presentation_id.config(fg='black')
+
+def on_focusout(event):
+    """Fonction appelée lorsque le champ de saisie perd le focus."""
+    if entry_presentation_id.get() == '':
+        entry_presentation_id.insert(0, 'Entrez l\'ID de la présentation...')
+        entry_presentation_id.config(fg='grey')
+
+# Mise à jour de l'interface graphique
 root = tk.Tk()
-root.title("Correcteur de Présentation")
-root.geometry("800x300")  # Largeur x Hauteur
+root.title("SlidesCleaner")
+root.geometry("750x800")  # Largeur x Hauteur ajustée
+
+# Création d'un Frame pour le champ de saisie et le bouton
+input_frame = tk.Frame(root)
+
+# Style de la "console"
+style = {'bg': 'black', 'fg': 'green', 'insertbackground': 'white', 'bd': 0}
 
 # Création des widgets
-label = tk.Label(root, text="Entrez l'ID de la présentation Google Slides :")
-entry_presentation_id = tk.Entry(root)
-submit_button = tk.Button(root, text="Corriger", command=on_submit)
-text_area = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=40, height=10)
+entry_presentation_id = tk.Entry(input_frame, fg='grey')
+entry_presentation_id.insert(0, 'Entrez l\'ID de la présentation...')
+entry_presentation_id.bind('<FocusIn>', on_entry_click)
+entry_presentation_id.bind('<FocusOut>', on_focusout)
+
+submit_button = tk.Button(input_frame, text="Corriger", command=on_submit)
+
+# Style de la "console"
+style = {'bg': 'black', 'fg': 'green', 'insertbackground': 'white', 'bd': 0}
+
+# Création de la zone de texte avec le style défini
+text_area = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=40, height=10, **style)
+text_area.pack(expand=True, fill='both', padx=10, pady=5)
+
+# Configuration des balises pour la coloration du texte
+text_area.tag_config('normal', foreground='white')
+text_area.tag_config('corrected', foreground='green')
+
+# En-tête ASCII
+ascii_header = """
+   _____ _      _____ _____  ______    _____ _      ______          _   _ ______ _____  
+  / ____| |    |_   _|  __ \|  ____|  / ____| |    |  ____|   /\   | \ | |  ____|  __ \ 
+ | (___ | |      | | | |  | | |__    | |    | |    | |__     /  \  |  \| | |__  | |__) |
+  \___ \| |      | | | |  | |  __|   | |    | |    |  __|   / /\ \ | . ` |  __| |  _  / 
+  ____) | |____ _| |_| |__| | |____  | |____| |____| |____ / ____ \| |\  | |____| | \ \ 
+ |_____/|______|_____|_____/|______|  \_____|______|______/_/    \_\_| \_|______|_|  \_\
+
+
+ ________________________________________________________________________________________
+"""
+
+# Ajouter l'en-tête ASCII à la zone de texte lors de l'initialisation de l'interface graphique
+text_area.insert('1.0', ascii_header)
+
+
 
 # Mise en page
-label.pack(fill='x', padx=10, pady=5)
-entry_presentation_id.pack(fill='x', padx=10, pady=5)
-submit_button.pack(fill='x', padx=10, pady=5)
+entry_presentation_id.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=5, pady=5)
+submit_button.pack(side=tk.LEFT, padx=10, pady=5)
+input_frame.pack(fill=tk.X, padx=10, pady=5)
 text_area.pack(expand=True, fill='both', padx=10, pady=5)
+
 
 
 # Initialisation de service dans le scope global
@@ -148,6 +220,7 @@ service = build('slides', 'v1', credentials=creds)
 
 # Démarrage de l'application
 root.mainloop()
+
 
 def main():
     presentation_id = input("Entrez l'ID de la présentation Google Slides : ")
